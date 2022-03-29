23ea013d97a0304cb0c271ece74a9fb7bc939b9b
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2022-03-29 17:00:12 -0600

    Edit Action for Category

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -43,5 +43,43 @@ namespace BulkyBookWeb.Controllers
 			}
             return View();
 		}
+
+        public IActionResult Edit(int? id)
+		{
+            if (id == null || id == 0)
+			{
+                return NotFound();
+			}
+
+            var category = _db.Categories.Find(id);
+            //var category = _db.Categories.SingleOrDefault(c => c.DisplayOrder == id);
+            //var category = _db.Categories.FirstOrDefault(c => c.DisplayOrder == id);
+
+            if (category == null)
+            { 
+                return NotFound(); 
+            }
+
+            return View(category);
+		}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+		{
+            if (obj.Name == obj.DisplayOrder.ToString())
+			{
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+			}
+            if (ModelState.IsValid)
+			{
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+			}
+            return View(obj);
+		}
+
+
     }
 }
